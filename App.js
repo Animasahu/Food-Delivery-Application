c8696e67165b0f87048f4484177ef356be0bab1f
Sahu <anima.sahu@capgemini.com>
2023-10-31 14:42:23 +0530

    functional component

--- App.js
+++ App.js
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom/client";
 
 //React Element
 //JSX - Babel is transpiles to a React.createElement => ReactElement-js object => HTMLElement(render)
-const heading = (
-  <h1 id="heading">React </h1>
+const Heading = () => (
+  <h1 id="head">React </h1>
 );
+
+//React functional Component
+
+const HeadingComponent = () => (
+  <div id="container">
+   <Heading/>
+    <h1 className="heading">Functional component</h1>
+  </div>
+
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent/>);
