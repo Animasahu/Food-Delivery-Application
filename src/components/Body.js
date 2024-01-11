7904a619d623b6eed35b284655461957c2b232e4
Sahu <anima.sahu@capgemini.com>
2024-01-11 17:06:14 +0530

    fetching data from swiggy API , create search bar , filtered top rated restaurant ,

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,76 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  
-  //local state variable 
-  const [listOfRestaurants , setListOfRestaurants] = useState(resList);
+  //local state variable
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurants] = useState([]);
 
-  return (
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body rendered");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+  " https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+   );
+    //https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING
+    //      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+
+
+    const json = await data.json();
+
+    console.log(json);
+
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-bttn"
+            onClick={() => {
+              //filtered the Restaurant cards and update the UI
+              // SearchText
+              console.log(searchText);
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurants(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4
             );
-            setListOfRestaurants(filteredList);
+            setFilteredRestaurants(filteredList);
             console.log(listOfRestaurants);
           }}
         >
@@ -24,7 +78,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,35 @@
-import {LOGO_UGL} from "../utils/constants"
+import { LOGO_UGL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={LOGO_UGL}
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li> Home </li>
-            <li> About Us </li>
-            <li> Contact Us </li>
-            <li> Cart </li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
+  const [btnNameReact, setBtnNameReact] = useState("login");
+  console.log("header rendered");
 
-  export default Header;
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_UGL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li> Home </li>
+          <li> About Us </li>
+          <li> Contact Us </li>
+          <li> Cart </li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "login"
+                ? setBtnNameReact("logout")
+                : setBtnNameReact("login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
